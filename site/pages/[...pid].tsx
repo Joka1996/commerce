a41691132eb2c9bdab0bac103a5703ad4075b119
Joel Karlsson <Joel.Karlsson@columbusglobal.com>
2022-06-08 08:50:27 +0200

    test6 deployment

--- site/pages/[...pid].tsx
+++ site/pages/[...pid].tsx
@@ -88,11 +88,14 @@ import type {
     return router.isFallback ? (
       <h1>Loading...</h1>
     ) : 
-    ( (data.content.productItem != null) ? (
-      //ändrat product till den befintliga och lagt till data
+    ( 
        <ProductView product={data.content.productItem} relatedProducts={relatedProducts} data={data} />
-       )
-       : console.log("Error, productItem null")
+
+      // (data.content.productItem != null) ? (
+      // //ändrat product till den befintliga och lagt till data
+      //  <ProductView product={data.content.productItem} relatedProducts={relatedProducts} data={data} />
+      //  )
+      //  : console.log("Error, productItem null")
     )
   }
 Woman.Layout = Layout
