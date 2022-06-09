aac7a0084ff2d5bc26e6bf62a3daf994f7ef2bb2
Joel Karlsson <Joel.Karlsson@columbusglobal.com>
2022-06-09 11:12:45 +0200

    test igen

--- site/components/product/ProductSidebar/ProductSidebar.tsx
+++ site/components/product/ProductSidebar/ProductSidebar.tsx
@@ -1,5 +1,5 @@
 import s from './ProductSidebar.module.css'
-import { useAddItem } from '@framework/cart'
+// import { useAddItem } from '@framework/cart'
 import { FC, useEffect, useState } from 'react'
 import { ProductOptions } from '@components/product'
 import type { Product } from '@commerce/types/product'
