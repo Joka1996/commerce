30663bb060e9226861e1592d79a5db1f1a30e3ff
Joel Karlsson <Joel.Karlsson@columbusglobal.com>
2022-06-09 11:45:08 +0200

    deployment test7

--- packages/local/src/provider.ts
+++ packages/local/src/provider.ts
@@ -3,7 +3,7 @@ import { fetcher } from './fetcher'
 // import { handler as useAddItem } from './cart/use-add-item'
 import { handler as useUpdateItem } from './cart/use-update-item'
 import { handler as useRemoveItem } from './cart/use-remove-item'
-import { handler as useCustomer } from './customer/use-customer'
+// import { handler as useCustomer } from './customer/use-customer'
 import { handler as useSearch } from './product/use-search'
 import { handler as useLogin } from './auth/use-login'
 // import { handler as useLogout } from './auth/use-logout'
@@ -14,7 +14,7 @@ export const localProvider = {
   cartCookie: 'session',
   fetcher: fetcher,
   // cart: { useCart, useAddItem, useUpdateItem, useRemoveItem },
-  customer: { useCustomer },
+  // customer: { useCustomer },
   products: { useSearch },
   // auth: { useLogin, useLogout, useSignup },
 }
